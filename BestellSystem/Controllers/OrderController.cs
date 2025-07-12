c8cf72d00f4d8a325da593afcb0c574334b4015e
malex1306 <alexandremarcel350@gmail.com>
2025-07-12 12:05:12 +0200

    Add project files.

---
+++ BestellSystem/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using BestellSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestellSystem.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : Controller
+    {
+        private static List<OrderModel> _orders = new();
+
+        [HttpGet]
+        public IActionResult AllOrders()
+        {
+            return Ok(_orders);
+        }
+
+        [HttpPost]
+        public IActionResult NewOrder([FromBody] OrderModel order)
+        {
+            _orders.Add(order);
+            return Ok();
+        }
+    }
+}
---
+++ BestellSystem/Models/OrderModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BestellSystem.Models
+{
+    public class OrderModel
+    {
+        public Guid Id { get; set; }
+        public string Costumer { get; set; }
+        public DateTime OrderTime { get; set; }
+    }
+}
